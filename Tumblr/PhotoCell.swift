748c83aecb31997adec509cb4fb2305db37eef59
Claudiazhaoya <sun515@purdue.edu>
2017-11-12 11:22:20 -0500

    lab complete

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Zhaoya Sun on 11/11/17.
+//  Copyright © 2017 CodePath University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        return super.awakeFromNib()
+    }
+}
